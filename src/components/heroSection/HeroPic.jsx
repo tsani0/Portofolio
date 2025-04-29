92d6e950faac1cc68e96b72442ce438de3d25c27
Hasnatsaniya <142221438+tsani0@users.noreply.github.com>
2025-04-29 09:33:41 +0700

    Update HeroPic.jsx
    Signed-off-by: Hasnatsaniya  <142221438+tsani0@users.noreply.github.com>

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { PiHexagonThin } from "react-icons/pi";
+import hexaaImg from '/images/hexaa.png';
 
 const HeroPic = () => {
   return (
     <div className='w-full flex items-center justify-center'>
-        <img src={require('../../public/images/hexaa.png')} alt="Hasna Tsaniya" className='max-h-[1200px] h-auto sm:h-[450px]' />
+        <img src={hexaaImg} alt="Hasna Tsaniya" className='max-h-[1200px] h-auto sm:h-[450px]' />
         <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
             <PiHexagonThin className='md:-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]' />
         </div>
